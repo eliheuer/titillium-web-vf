7e9d22149c3a05ac7a9627dd14d578bbfbecc8a4
Eli Heuer <eliheuer@gmail.com>
2018-11-08 13:54:33 -0500

    Update source files and output

--- docs/drawbot-sources/basic-specimen.py
+++ docs/drawbot-sources/basic-specimen.py
@@ -3,22 +3,20 @@ from drawBot import *
 import math
 import os
 
-
 # STATIC VARIABLE
 W,H,M,F = 1000,1000,100,30     # WIDTH, HEIGHT, MARGIN, FRAMES
-VAR_WGHT = 30                  # VARIABLE FONT WEIGHT
+VAR_WGHT = 100                 # VARIABLE FONT WEIGHT
 LINE_H = H/10                  # LINE HEIGHT
 START_POS = 854
 
-
 # SET FONT
-font("fonts/Titillium-Web-VF.ttf")
+font("fonts/Titillium-Web-Roman-VF.ttf")
 for axis, data in listFontVariations().items():
     print((axis, data))
 
 # GRID DRAWING FUNCTION
 def grid(inc):
-    stroke(0.5)
+    stroke(0.2)
     stpX, stpY = 0, 0
     incX = (W-(M*2))/inc
     incY = (W-(M*2))/inc
@@ -42,15 +40,26 @@ tracking(0)
 stroke(None)
 fontSize(100)
 fontVariations(wght=900)
-text("Titillium Web",      (M+10, (START_POS)-(0*LINE_H)))
+text("Titillium Web",      (M, (START_POS)-(0*LINE_H)))
+
+
+fontSize(400)
+fontVariations(wght=900)
+text("a",      (M+390, (START_POS+225)-(4*LINE_H)))
+fontVariations(wght=200)
+text("a",      (M+610, (START_POS+225)-(4*LINE_H)))
+
 
 # Draw large type
-fontSize(50)
+fontSize(40)
+tracking(2)
 for i in range(6):
-    VAR_WGHT += 40
+    VAR_WGHT += 100
     fontVariations(wght=VAR_WGHT)
     print("VAR_WGHT=", VAR_WGHT) 
-    text("Variable Font", (M+10, (START_POS-M+3)-(i*40)))
+    text("Variable Font: %s" %VAR_WGHT, (M, (START_POS-M)-(i*40)))
+tracking(0)
+
 
 # Dot
 fill(1,0,0)
@@ -59,26 +68,23 @@ oval(M, M, (W-(M*2))/20, (W-(M*2))/20)
 # Draw small type
 fontVariations(wght=400)
 fill(1)
+fontSize(40)
+text("A B C D E F G H I J K L M ",            (M+400, (START_POS-M)-( 7*40)))
+text("N O P Q R S T U V W X Y",               (M+400, (START_POS-M)-( 8*40)))
+text("& Z 1 2 3 4 5 6 7 8 9 0",               (M+400, (START_POS-M)-( 9*40)))
+text("a b c d e f g h i j k l m n o",         (M+400, (START_POS-M)-(10*40)))
+text("p q r s t u v w x y z",                 (M+400, (START_POS-M)-(11*40)))
+tracking(-2)
 fontSize(50)
-tracking(-4)
-text("A B C D E F G H I J ",      (M+440, (START_POS)-( 8*LINE_H/2)))
-text("K L M N O P Q R S T ",      (M+440, (START_POS)-( 9*LINE_H/2)))
-text("U V W X Y Z ",              (M+440, (START_POS)-(10*LINE_H/2)))
-text("a b c d e f g h i j k l",   (M+440, (START_POS)-(11*LINE_H/2)))
-text("m n o p q r s t u v",       (M+440, (START_POS)-(12*LINE_H/2)))
-text("w x y z",                   (M+440, (START_POS)-(13*LINE_H/2)))
-text("w x y z",                   (M+440, (START_POS)-(13*LINE_H/2)))
-
-text(" ز ر ذ د خ ح ج ث ت ب ا ",   (M, (START_POS)-( 8*LINE_H/1.5)))
-text(" غ ع ظ ط ض ص ش س ",     (M, (START_POS)-( 9*LINE_H/1.5)))
-text(" ي و ه ن م ل ك ق ف ",       (M, (START_POS)-(10*LINE_H/1.5)))
-text(" ه ه ه ﺏ ﺏ ﺏ ",             (M, (START_POS)-(13*LINE_H/1.5)))
+text(" ذ د خ ح ج ث ت ب ا "[::-1],         (M+26,  (START_POS+92)-( 6*80)))
+text(" ﻅ ظ ط ض ص ش س  "[::-1],             (M+2, (START_POS+92)-( 7*80)))
+text(" ن م ل ك ق ف غ ع "[::-1],           (M+60, (START_POS+92)-( 8*80)))
 
 # Save GIF
 os.chdir("docs")
 os.chdir("specimens")
 saveImage("basic-specimen.gif")
-saveImage("basic-specimen.pdf")
-saveImage("basic-specimen.png")
+# saveImage("basic-specimen.pdf")
+# saveImage("basic-specimen.png")
 os.chdir("..")
 os.chdir("..")
--- sources/BUILD.py
+++ sources/BUILD.py
@@ -6,13 +6,16 @@ from fontTools.ttLib import TTFont
 # STATIC VARIABLES
 # SOURCES is a list of the source names you want to build, without file extensions:
 #SOURCES = ['Titillium-Web-Regular', 'Titillium-Web-Italic']
-SOURCES = ['Titillium-Web-Regular']
+SOURCES = ['Titillium-Web-Roman']
+OUTPUT = "TitilliumWeb"
+NEWFONT_ONE = "Titillium-Web-Roman"
+NEWFONT_TWO = "Titillium-Web-Italic"
 
 # START SCRIPT
 print("\n**** Starting FAFR (Fully Automated Font Repository) build process:")
 for source in SOURCES:
     time.sleep(0.5)
-    print("     [+] Sources: Preparing to build ", source)
+    print("     [+] Sources: Preparing to build", source)
 
 # FONTMAKE
 for source in SOURCES:
@@ -20,22 +23,20 @@ for source in SOURCES:
         print("\n**** Building %s font files with Fontmake:" %source)
         print("     [+] Run: fontmake -g sources/%s.glyphs -o variable" %source)
         subprocess.call("fontmake -g sources/%s.glyphs -o variable" %source, shell=True)
-        subprocess.call("fontmake -g sources/%s.glyphs -o variable > /dev/null 2>&1" %source, shell=True)
+        #subprocess.call("fontmake -g sources/%s.glyphs -o variable > /dev/null 2>&1" %source, shell=True)
         print("     [!] Done")
     except:
         print("     [?] Error! Try installing Fontmake: https://github.com/googlei18n/fontmake")
 
-quit()
-
 # MOVE FONTS
 print("\n**** Moving %s font to the fonts directory" %source)
-subprocess.call("cp variable_ttf/%s-VF.ttf fonts/%s-VF.ttf" % (FONT, FONT), shell=True)
+subprocess.call("cp variable_ttf/%s-VF.ttf fonts/%s-VF.ttf" % (OUTPUT, NEWFONT_ONE), shell=True)
 print("     [+] Done")
 
 # CLEANUP
 print("\n**** Removing build directories")
-print("     [+] Run: rm -rf variable_ttf master_ufo")
-subprocess.call("rm -rf variable_ttf master_ufo", shell=True)
+print("     [+] Run: rm -rf variable_ttf master_ufo instance_ufo")
+subprocess.call("rm -rf variable_ttf master_ufo instance_ufo", shell=True)
 print("     [+] Done")
 
 # AUTOHINT
@@ -43,9 +44,10 @@ print("\n**** Run: ttfautohint")
 os.chdir('fonts')
 cwd = os.getcwd()
 print("     [+] In Directory:", cwd)
-subprocess.call("ttfautohint -I %s-VF.ttf %s-VF-Fix.ttf" %(FONT, FONT), shell=True)
-subprocess.call("cp %s-VF-Fix.ttf %s-VF.ttf" %(FONT, FONT), shell=True)
-subprocess.call("rm -rf %s-VF-Fix.ttf" %FONT, shell=True)
+for source in SOURCES:
+    subprocess.call("ttfautohint -I %s-VF.ttf %s-VF-Fix.ttf" %(source, source), shell=True)
+    subprocess.call("cp %s-VF-Fix.ttf %s-VF.ttf" %(source, source), shell=True)
+    subprocess.call("rm -rf %s-VF-Fix.ttf" %source, shell=True)
 print("     [+] Done")
 
 # GFTOOLS
@@ -53,18 +55,20 @@ print("\n**** Run: gftools")
 os.chdir("..")
 cwd = os.getcwd()
 print("     [+] In Directory:", cwd)
-#subprocess.call("gftools fix-dsgi fonts/Foo-VF.ttf --autofix", shell=True)
+for source in SOURCES:
+    subprocess.call("gftools fix-dsig fonts/%s-VF.ttf --autofix" %source, shell=True)
+print("     [+] Done")
 
 # FONTTOOLS
-print("\n**** Run: edit xAvgCharWidth")
+print("\n**** Run: fonttools -- edit xAvgCharWidth")
 cwd = os.getcwd()
 print("     [+] In Directory:", cwd)
 #font = TTFont('fonts/$font-VF.ttf')
-print("     [+] font =", FONT)
+#print("     [+] font =", FONT)
 print("     [+] Done")
 
 # DRAWBOT
 print("\n**** Run: DrawBot")
-#subprocess.call("python3 docs/drawbot-sources/basic-specimen.py > /dev/null 2>&1", shell=True)
+subprocess.call("python3 docs/drawbot-sources/basic-specimen.py", shell=True)
 print("     [+] Done")
 
