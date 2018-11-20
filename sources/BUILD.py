62899ae84bd3ec04c305d0a7c0cc1d10f899111b
Eli Heuer <eliheuer@gmail.com>
2018-11-20 12:09:52 -0500

    Update build script: send noise output to dev/null

--- sources/BUILD.py
+++ sources/BUILD.py
@@ -18,9 +18,9 @@ def check_root_dir():
     repo_test = os.listdir(path='.')
     repo_test_result =  all(elem in repo_test for elem in REPO_ROOT)
     if repo_test_result:
-        print("\n     [+] OK: Looks good")
+        print("     [+] OK: Looks good")
     else:
-        print("\n     [!] ERROR: Please run this script from the root directory of a git repo.")
+        print("     [!] ERROR: Please run this script from the root directory of a git repo.")
 
 def get_source_list():
     """
@@ -31,7 +31,7 @@ def get_source_list():
     for name in glob.glob('*.glyphs'):
         sources.append(os.path.splitext(name)[0])
     os.chdir('..')
-    print("\n     [+] SOURCES: List of sources =", sources)
+    print("     [+] SOURCES: List of sources =", sources)
 
 def get_style_list():
     """
@@ -40,11 +40,11 @@ def get_style_list():
     print("\n**** Starting FAFR (Fully Automated Font Repository) build process:")
     for source in sources:
         time.sleep(0.5)
-        print("\n     [+] SOURCES: Preparing to build", source)
+        print("     [+] SOURCES: Preparing to build", source)
         print("     [+] SOURCES: Style =", source.rpartition('-')[2])
         sources_style = str(source.rpartition('-')[2])
         sources_styles.append(sources_style)
-    print("\n     [+] SOURCES: Styles =", sources_styles)
+    print("     [+] SOURCES: Styles =", sources_styles)
 
 def run_fontmake():
     """
@@ -55,7 +55,7 @@ def run_fontmake():
         print("     [+] Run: fontmake -g sources/%s.glyphs -o variable --overlaps-backend booleanOperations --output-path fonts/%s-VF.ttf" %(source, source) )
         #subprocess.call("fontmake -g sources/%s.glyphs -o variable --output-path fonts/%s-VF.ttf" %(source, source), shell=True)
         subprocess.call("fontmake -g sources/%s.glyphs -o variable --overlaps-backend booleanOperations --output-path fonts/%s-VF.ttf > /dev/null 2>&1" %(source, source), shell=True)
-        print("\n     [!] Done")
+        print("     [!] Done")
 
 def rm_build_dirs():
     """
@@ -90,7 +90,7 @@ def fix_dsig():
     cwd = os.getcwd()
     print("     [+] In Directory:", cwd)
     for source in sources:
-        subprocess.call("gftools fix-dsig fonts/%s-VF.ttf --autofix" %source, shell=True)
+        subprocess.call("gftools fix-dsig fonts/%s-VF.ttf --autofix > /dev/null 2>&1" %source, shell=True)
         print("     [+] Done:", source)
     print("     [+] Done")
 
@@ -99,7 +99,7 @@ def render_specimens():
     Render specimens
     """
     print("\n**** Run: DrawBot")
-    subprocess.call("python3 docs/drawbot-sources/basic-specimen.py", shell=True)
+    subprocess.call("python3 docs/drawbot-sources/basic-specimen.py > /dev/null 2>&1", shell=True)
     print("     [+] Done")
 
 if __name__ == '__main__':
