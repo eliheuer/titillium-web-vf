015ab9006b89ed0d8931ea0483649edc1ab6433c
Eli Heuer <eliheuer@gmail.com>
2019-03-13 12:37:30 -0400

    Update build script for PR to GF

--- BUILD.sh
+++ BUILD.sh
@@ -2,8 +2,8 @@
 python3 sources/BUILD.py \
     --ttfautohint "-v --stem-width-mode=qsq" \
     --static \
-#    --googlefonts ~/Google/fonts/ofl/titilliumweb \
-#    --fontbakery \
+    --googlefonts ~/Google/fonts/ofl/titilliumweb \
+    --fontbakery \
 #    --drawbot \
 #    --addfont \
 #    --fixnonhinting
