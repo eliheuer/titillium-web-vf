f959e9f750c7c4c9290e7f9b2a8f8dfddbae7a90
Eli Heuer <eliheuer@gmail.com>
2019-03-21 20:54:45 -0400

    update build script

--- BUILD.sh
+++ BUILD.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 python3 sources/BUILD.py \
     --ttfautohint "-v --stem-width-mode=qsq" \
-    --static \
-    --googlefonts ~/Google/fonts/ofl/titilliumweb \
-    --fontbakery \
+    --googlefonts ~/Google/fonts/ofl/titilliumweb
+#    --static \
+#    --fontbakery \
 #    --drawbot \
 #    --addfont \
 #    --fixnonhinting
