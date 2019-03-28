c718586ffc06866e0a483952ebce51627a7a2eca
Eli Heuer <eliheuer@gmail.com>
2019-03-28 19:46:33 -0400

    Update build info

--- BUILD.sh
+++ BUILD.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 python3 sources/BUILD.py \
     --ttfautohint "-v --stem-width-mode=qsq" \
-    --googlefonts ~/Google/fonts/ofl/titilliumweb
-#    --static \
+    --googlefonts ~/Google/fonts/ofl/titilliumweb \
+    --static
 #    --fontbakery \
 #    --drawbot \
 #    --addfont \
