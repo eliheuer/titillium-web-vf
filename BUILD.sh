20b43147d58d31820d88325069a81414aabb47ff
Eli Heuer <elih@protonmail.com>
2019-12-04 15:03:42 -0500

    Remove unused files

--- BUILD.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-python3 sources/BUILD.py \
-    --ttfautohint "-v --stem-width-mode=qsq" \
-    --googlefonts ~/Google/fonts/ofl/titilliumweb \
-    --static
-#    --fontbakery \
-#    --drawbot \
-#    --addfont \
-#    --fixnonhinting
